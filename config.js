aec084b1f71544cd59e6af0e03e16b9c1790d284
perencanaandkpppblg-create <perencanaandkpppblg@gmail.com>
2026-07-08 12:54:22 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // ISI DUA NILAI DI BAWAH DENGAN DATA DARI PROYEK SUPABASE ANDA
 // Supabase Dashboard -> Project Settings -> API
 // ============================================================
-window.SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+window.SUPABASE_URL = "https://hexhmtubabxwfdnfosrs.supabase.co/v1";
+window.SUPABASE_ANON_KEY = "sb_secret_-XhaSbUFnMwlSX_UfUJaug_SxB40hHp";
