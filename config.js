a16e3d4194e4d3710201170aa7c8eedfe8fdaec0
perencanaandkpppblg-create <perencanaandkpppblg@gmail.com>
2026-07-08 13:43:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // ISI DUA NILAI DI BAWAH DENGAN DATA DARI PROYEK SUPABASE ANDA
 // Supabase Dashboard -> Project Settings -> API
 // ============================================================
-window.SUPABASE_URL = "https://hexhmtubabxwfdnfosrs.supabase.co/v1";
+window.SUPABASE_URL = "https://hexhmtubabxwfdnfosrs.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_K9hKDZDcBft8E0ng_ZlQtA_Io68OvSE";
